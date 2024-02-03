76e406c6cfb86b3a37faeba532653f163cd3e04b
adramelech-123 <tmamvuto6@gmail.com>
2024-02-03 19:42:56 +0200

    Config and Routes setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,21 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        purplePrimary: "#291C3A",
+        purpleSecondary: "#331C52",
+        purpleTertiary: "8121D0",
+        purpleTint: "#DCCFED",
+        purpleFeint: "E1EBED"
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
     },
   },
   plugins: [],
